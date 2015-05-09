2c2cd1e4b06668cd5b5747d150630d310dbf9f78
Matth900 <1404268@studbocconi.it>
2015-05-09 21:35:57 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,10 @@
+# Code for making the first Plot
+# Plot Description : Histogram reporting frequencies for Global Active Power within the two dates 01/02/2007-02/02/2007
 
+source("load_file.R") # First of all, we need to load the data, sourcing the load_file.R script
+
+png(file="plot1.png",width=480,height=480)
+
+with(data,hist(Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)"))
+
+dev.off()
